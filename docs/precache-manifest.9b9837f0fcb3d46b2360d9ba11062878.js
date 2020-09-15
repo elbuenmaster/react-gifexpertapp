55a9dea4116c763af41ccc6501f170b443f28a15
Oscar Aguilera <aguilera.net>
2020-09-15 15:49:12 -0300

    docs agregado

---
+++ docs/precache-manifest.9b9837f0fcb3d46b2360d9ba11062878.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972283c03c4311c64c578894a67d3014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e430397d190c7852b1b3",
+    "url": "/static/css/main.8b9ff536.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e430397d190c7852b1b3",
+    "url": "/static/js/main.ea150e7f.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
